ed8b584d022efbfe77ba9d1bb635bdb8a2adedd0
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-09 11:21:57 +0530

    Blog UI

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    express     = require("express"),
-    app         = express();
+var expressSanitizer = require("express-sanitizer"),
+    bodyParser       = require("body-parser"),
+    methodOverride   = require("method-override"),
+    mongoose         = require("mongoose"),
+    express          = require("express"),
+    app              = express();
 
 //APP CONFIG
 mongoose.Promise = global.Promise;
@@ -11,6 +13,8 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 
 //SCHEMA SETUP
@@ -47,6 +51,7 @@ app.get("/blogs/new", function(req, res) {
 
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body); //Removes Script tag
   //create blog
   Blog.create(req.body.blog, function(err, newBlog) {
     if(err) {
@@ -70,7 +75,41 @@ app.get("/blogs/:id", function(req, res) {
   });
 });
 
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
 
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body); //Removes Script tag
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) { //(id, newData, callback)
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+  //destroy blog
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      //redirect somewhere
+      res.redirect("/blogs");
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("SERVER IS RUNNING");
