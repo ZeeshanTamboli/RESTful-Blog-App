720208474d8c0e8b0f30f449ae3b090c694eb2db
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-11 12:49:41 +0530

    add mongolab

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ var expressSanitizer = require("express-sanitizer"),
     app              = express();
 
 const port = process.env.PORT || 3000;
-var url = process.env.MONGOLAB_URI;
+//"mongodb://localhost:27017/restful_blog_app"
 
 //APP CONFIG
-mongoose.Promise = global.Promise;
-mongoose.connect(url || "mongodb://localhost:27017/restful_blog_app", {
+mongoose.connect("mongodb://ZeeshanTamboli:reshuaunty@ds117615.mlab.com:17615/restful_blog_app", {
   useMongoClient: true,
 });
 app.use(bodyParser.urlencoded({extended: true}));
